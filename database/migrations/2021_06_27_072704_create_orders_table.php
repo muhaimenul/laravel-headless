95171d962d8648f6cae444bc308fdc7299e8825d
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2021-06-27 21:07:01 +0600

    3+4.3 table and model for csv data import added

--- database/migrations/2021_06_27_072704_create_orders_table.php
+++ database/migrations/2021_06_27_072704_create_orders_table.php
@@ -15,6 +15,20 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('region');
+            $table->string('country');
+            $table->string('item_Type');
+            $table->string('sales_channel');
+            $table->string('order_priority');
+            $table->string('order_date');
+            $table->string('order_id');
+            $table->string('ship_date');
+            $table->string('units_sold');
+            $table->string('unit_price');
+            $table->string('unit_cost');
+            $table->string('total_revenue');
+            $table->string('total_cost');
+            $table->string('total_profit');
             $table->timestamps();
         });
     }
