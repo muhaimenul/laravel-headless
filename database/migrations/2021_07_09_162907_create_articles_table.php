eb8c2c5629fe50b370d446b340a6ba89131a8861
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2021-07-09 23:41:04 +0600

    article table done

--- database/migrations/2021_07_09_162907_create_articles_table.php
+++ database/migrations/2021_07_09_162907_create_articles_table.php
@@ -15,6 +15,21 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->string('title');
+            $table->string('banner')->nullable();
+            $table->string('slug')->unique();
+            $table->longText('body');
+
+            $table->datetime('published_at')->nullable();
+            $table->tinyInteger('status')->default(1)->comment('0 inactive, 1 active');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
