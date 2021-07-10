ae5c16b884607963505cde4c5bdc6b3366959ccd
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2021-07-10 20:28:00 +0600

    login api done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         $user = $this->userSvc->createUser($validatedData);
@@ -31,6 +31,32 @@ class AuthController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'Register successfully!',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!auth()->attempt($validatedData)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = $this->userSvc->model->where('email', $validatedData['email'])->firstOrFail();
+
+        $token = $this->userSvc->createToken($user);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logged in successfully!',
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
